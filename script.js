55c5c68835047a888506a5d51e41978dbde5a626
Andrés Felipe Vargas Fierro <89603799+CaponAF@users.noreply.github.com>
2021-09-12 17:00:53 -0500

    efecto

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 $(document).ready(
-    
-    function() {
 
-    $padre = $('#seccion0');
-    $hija =  $('#seccion0 .efecto');
-    
-    $hija.each(
-    function(){
-        
-        var elemento =$(this);
-       
-        if(elemento.hasClass('e-derecha')){
-            elemento.css({right: '0px',  filter: 'opacity(0%)'});
-        alert('entro')
-        }
-        if(elemento.hasClass('e-izquierda')){
-            elemento.css({left: '0px',  filter: 'opacity(0%)'});
-        }
-        if(elemento.hasClass('e-arriba')){
-            elemento.css({top: '0px',  filter: 'opacity(0%)'});
-        }
-        if(elemento.hasClass('e-abajo')){
-            elemento.css({bottom: '0px',  filter: 'opacity(0%)'});
-        }
+    function () {
+    deslizar( $('#seccion0'), $('#seccion0 .efecto') );
+    deslizar( $('#seccion0'), $('#seccion0 .efecto') );
+ 
+
+
     }
 
-    );
-
-
-}
-
 );
+
+
+function deslizar( padre, hija) {
+     
+
+    hija.each(
+        function () {
+
+            var elemento = $(this);
+
+            if(elemento.hasClass('e-derecha')) {
+                elemento.css({ right: '0px', filter: 'opacity(100%)' });
+
+            }
+            if(elemento.hasClass('e-izquierda')) {
+                elemento.css({ left: '0px', filter: 'opacity(100%)' });
+            }
+            if(elemento.hasClass('e-arriba')) {
+                elemento.css({ top: '0px', filter: 'opacity(100%)'});
+            }
+            if(elemento.hasClass('e-abajo')) {
+                elemento.css({ bottom: '0px', filter: 'opacity(100%)'});
+            }
+        }
+
+    );
+}
